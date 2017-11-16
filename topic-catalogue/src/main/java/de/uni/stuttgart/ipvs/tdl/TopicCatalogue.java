6fe342b38cb4b6f8156ee8bde3e2d2a24517bf5f
Tobias Angerstein <angersts@studi.informatik.uni-stuttgart.de>
2017-11-16 10:21:46 +0100

    Add initial packet structure

--- topic-catalogue/src/main/java/de/uni/stuttgart/ipvs/tdl/TopicCatalogue.java
+++ topic-catalogue/src/main/java/de/uni/stuttgart/ipvs/tdl/TopicCatalogue.java
@@ -1,12 +1,12 @@
-package com.example.demo;
+package de.uni.stuttgart.ipvs.tdl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class TopicCatalogue {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(TopicCatalogue.class, args);
 	}
 }
---
+++ topic-catalogue/src/main/java/de/uni/stuttgart/ipvs/tdl/rest/TDLRestController.java
@@ -0,0 +1,10 @@
+package de.uni.stuttgart.ipvs.tdl.rest;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class TDLRestController {
+
+}
