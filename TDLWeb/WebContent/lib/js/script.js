93aa2dacb47acf1623b7e7f63e445d818299e88f
Simon Lehmann <lehmann_simon@gmx.de>
2018-08-21 10:47:42 +0200

    Changed json schema

--- TDLWeb/WebContent/lib/js/script.js
+++ TDLWeb/WebContent/lib/js/script.js
@@ -1,6 +1,6 @@
 var app = angular.module('tdlApp', []);
-app.controller('tdlCtrl', function($scope, $http) {
-	
+app.controller('tdlCtrl', function ($scope, $http) {
+
 	$scope.topicDescriptionProperties = [
 		"data_type",
 		"hardware_type",
@@ -13,171 +13,202 @@ app.controller('tdlCtrl', function($scope, $http) {
 		"path"
 	]
 
-	var serverUrl = "http://192.168.209.199:8080";
-	
-	$scope.swaggerUrl = serverUrl + "/swagger-ui.html";	
-	var url = serverUrl + "/catalogue";
-	
-	$http({
-    	method: 'POST', 
-    	url: url + "/search",
-    	data: { filters: {}},
-    	headers: { "Content-Type": "application/json" }
-    }).then(function(response) {
-    	$scope.status = response.status;    	
-    	$scope.topicDescription = [];
-    	
-    	for(var i in response.data){
-    		var elementData = response.data[i];
-    		$scope.topicDescription.push(angular.fromJson(elementData)); 
-    	}   
-    	
-    	$scope.backUpTopicDescription = JSON.parse(JSON.stringify($scope.topicDescription));
-    	
-    }, function(response) {
-        $scope.data = response.data || 'Request failed';
-        $scope.status = response.status;
-    });
-	
-	$scope.removeTopicDescription = function(topic) {
-        $scope.response = null;
+	var serverUrl = "http://localhost:8080";
 
-        $http({
-        	method: 'DELETE', 
-        	url: url + "/delete/" + topic._id.$oid
-        }).then(function(response) {
-	    	$scope.status = response.status;
-   			for(var i in $scope.topicDescription){
-   				if($scope.topicDescription[i]._id.$oid == topic._id.$oid){
-   					$scope.topicDescription.splice(i, 1);
-   					$scope.backUpTopicDescription.splice(i, 1);
-   					break;
-   				}
-	    	} 
-	    }, function(response) {
-	        $scope.data = response.data || 'Request failed';
-	        $scope.status = response.status;
-	    });
-	};
-	
-	$scope.cancelUpdateTopicDescription = function(topic) {      
-		for(var i in $scope.backUpTopicDescription){
-			if($scope.backUpTopicDescription[i]._id.$oid == topic._id.$oid){
-				$scope.topicDescription[i] = JSON.parse(JSON.stringify($scope.backUpTopicDescription[i]));
-				break;
-			}
-        } 
-	};
-	
-	$scope.updateTopicDescription = function(topic) {
+	$scope.swaggerUrl = serverUrl + "/swagger-ui.html";
+	var url = serverUrl + "/catalogue";
+
+	$http({
+		method: 'GET',
+		url: "https://api.github.com/repos/lehmansn/TDLPolicy/contents/policy",
+		data: { filters: {} },
+		headers: { "Content-Type": "application/json" }
+	}).then(function (response) {
+		for (var i in response.data) {
+			$http({
+				method: 'GET',
+				url: response.data[i].download_url,
+				data: { filters: {} },
+				headers: { "Content-Type": "text/plain" }
+			}).then(function (responsePolicy) {
+				var policyData = responsePolicy.data;
+				var tabContent = document.createElement("div")
+				var policyName = policyData.name.charAt(0).toUpperCase() + policyData.name.substr(1);
+				var policyDesc = policyData.description;
+				tabContent.className = "tabcontent";
+				tabContent.innerHTML = policyName + "\n" + policyDesc;
+				var tabButton = document.createElement("button");
+				tabButton.innerHTML = policyName;
+				tabButton.addEventListener("click", function () {
+					alert("<h3>" + policyName + "</h3>\n" + policyDesc)
+				});
+				document.getElementById("PolicyCatalog").appendChild(tabButton);
+			});
+		}
+	});
+
+	$http({
+		method: 'POST',
+		url: url + "/search",
+		data: { filters: {} },
+		headers: { "Content-Type": "application/json" }
+	}).then(function (response) {
+		$scope.status = response.status;
+		$scope.topicDescription = [];
+
+		for (var i in response.data) {
+			var elementData = response.data[i];
+			$scope.topicDescription.push(angular.fromJson(elementData));
+		}
+
+		$scope.backUpTopicDescription = JSON.parse(JSON.stringify($scope.topicDescription));
+
+	}, function (response) {
+		$scope.data = response.data || 'Request failed';
+		$scope.status = response.status;
+	});
+
+	$scope.removeTopicDescription = function (topic) {
 		$scope.response = null;
-        
-		var updateBody = {};
-		for(var key in topic){
-			if(key != "_id"){
-				updateBody[key] = topic[key];
-			}
-		}
-        
-		for(var i in $scope.backUpTopicDescription){
-			if($scope.backUpTopicDescription[i]._id.$oid == topic._id.$oid){
-				$scope.backUpTopicDescription[i] = JSON.parse(JSON.stringify(topic));
-				break;
-			}
-		} 
-        
+
 		$http({
-			method: 'PUT', 
-			url: url + "/update/" + topic._id.$oid,
-			data: updateBody,
-			headers: { "Content-Type": "application/json" }
-		}
-		).then(function(response) {
+			method: 'DELETE',
+			url: url + "/delete/" + topic._id.$oid
+		}).then(function (response) {
 			$scope.status = response.status;
-		}, function(response) {
+			for (var i in $scope.topicDescription) {
+				if ($scope.topicDescription[i]._id.$oid == topic._id.$oid) {
+					$scope.topicDescription.splice(i, 1);
+					$scope.backUpTopicDescription.splice(i, 1);
+					break;
+				}
+			}
+		}, function (response) {
 			$scope.data = response.data || 'Request failed';
 			$scope.status = response.status;
 		});
 	};
-	
-	$scope.cancelInsertTopicDescription = function() {      
-		for(var property in $scope.insert){
-			$scope.insert[property] = "";
-		}   
+
+	$scope.cancelUpdateTopicDescription = function (topic) {
+		for (var i in $scope.backUpTopicDescription) {
+			if ($scope.backUpTopicDescription[i]._id.$oid == topic._id.$oid) {
+				$scope.topicDescription[i] = JSON.parse(JSON.stringify($scope.backUpTopicDescription[i]));
+				break;
+			}
+		}
 	};
-	
-	$scope.insertTopicDescription = function() {
-        $scope.response = null;
-        
-        var topicDescription = {};
-        for(var property in $scope.insert){
-        	var value = $scope.insert[property];
-        	if(value != ""){
-        		topicDescription[property] = value;
-        	}
-    	}     
-        
-        if($.isEmptyObject(topicDescription)){
-        	alert('No values available! Please insert values.');
-        	return;
-        }
-	
-    	$http({
-        	method: 'POST', 
-        	url: url + "/add",
-        	data: topicDescription,
-        	headers: { "Content-Type": "text/plain" },
-        	transformResponse: [function (data) {
-       	      return data;
-       	  }]
-    	}
-        ).then(function(response) {
-	    	$scope.status = response.status;
-	    	$scope.idOfNewTopicDescription = response.data;
-	    	$scope.addDate = new Date();
-	    	topicDescription["_id"] = {};
-	        topicDescription["_id"]["$oid"] = $scope.idOfNewTopicDescription;
+
+	$scope.updateTopicDescription = function (topic) {
+		$scope.response = null;
+
+		var updateBody = {};
+		for (var key in topic) {
+			if ((key != "_id") && (topic[key] != "")) {
+				updateBody[key] = topic[key];
+			}
+		}
+
+		for (var i in $scope.backUpTopicDescription) {
+			if ($scope.backUpTopicDescription[i]._id.$oid == topic._id.$oid) {
+				$scope.backUpTopicDescription[i] = JSON.parse(JSON.stringify(topic));
+				break;
+			}
+		}
+
+		$http({
+			method: 'PUT',
+			url: url + "/update/" + topic._id.$oid,
+			data: updateBody,
+			headers: { "Content-Type": "application/json" }
+		}
+		).then(function (response) {
+			console.log("Update success (200)?");
+			$scope.status = response.status;
+		}, function (response) {
+			console.log("Update failed (400)?");
+			$scope.data = response.data || 'Request failed';
+			$scope.status = response.status;
+		});
+	};
+
+	$scope.cancelInsertTopicDescription = function () {
+		for (var property in $scope.insert) {
+			$scope.insert[property] = "";
+		}
+	};
+
+	$scope.insertTopicDescription = function () {
+		$scope.response = null;
+
+		var topicDescription = {};
+		for (var property in $scope.insert) {
+			var value = $scope.insert[property];
+			if (value != "") {
+				topicDescription[property] = value;
+			}
+		}
+
+		if ($.isEmptyObject(topicDescription)) {
+			alert('No values available! Please insert values.');
+			return;
+		}
+
+		$http({
+			method: 'POST',
+			url: url + "/add",
+			data: topicDescription,
+			headers: { "Content-Type": "text/plain" },
+			transformResponse: [function (data) {
+				return data;
+			}]
+		}
+		).then(function (response) {
+			$scope.status = response.status;
+			$scope.idOfNewTopicDescription = response.data;
+			$scope.addDate = new Date();
+			topicDescription["_id"] = {};
+			topicDescription["_id"]["$oid"] = $scope.idOfNewTopicDescription;
 			$scope.topicDescription.push(topicDescription);
 			$scope.backUpTopicDescription.push(topicDescription);
-	    }, function(response) {
-	        $scope.data = response.data || 'Request failed';
-	        $scope.status = response.status;
-	    });
+		}, function (response) {
+			$scope.data = response.data || 'Request failed';
+			$scope.status = response.status;
+		});
 	};
-	
-	$scope.getDataTypes = function(topicDescriptions) {
+
+	$scope.getDataTypes = function (topicDescriptions) {
 		var result = [];
 
-		angular.forEach(topicDescriptions, function(topic, key) {
+		angular.forEach(topicDescriptions, function (topic, key) {
 			var value = topic.data_type;
-			if(value != null && !result.includes(value)){
-				result.push(value);	
+			if (value != null && !result.includes(value)) {
+				result.push(value);
 			}
 		});
 
 		return result;
 	}
-	
-	$scope.getProtocol = function(topicDescriptions) {
+
+	$scope.getProtocol = function (topicDescriptions) {
 		var result = [];
 
-		angular.forEach(topicDescriptions, function(topic, key) {
+		angular.forEach(topicDescriptions, function (topic, key) {
 			var value = topic.protocol;
-			if(value != null && !result.includes(value)){
-				result.push(value);	
+			if (value != null && !result.includes(value)) {
+				result.push(value);
 			}
 		});
 
 		return result;
 	}
-	
-	$scope.getMessageFormat = function(topicDescriptions) {
+
+	$scope.getMessageFormat = function (topicDescriptions) {
 		var result = [];
 
-		angular.forEach(topicDescriptions, function(topic, key) {
+		angular.forEach(topicDescriptions, function (topic, key) {
 			var value = topic.message_format;
-			if(value != null && !result.includes(value)){
-				result.push(value);	
+			if (value != null && !result.includes(value)) {
+				result.push(value);
 			}
 		});
 
--- topic-catalogue/src/main/java/de/uni/stuttgart/ipvs/tdl/rest/TDLRestController.java
+++ topic-catalogue/src/main/java/de/uni/stuttgart/ipvs/tdl/rest/TDLRestController.java
@@ -1,6 +1,5 @@
 package de.uni.stuttgart.ipvs.tdl.rest;
 
-import static javax.swing.text.html.FormSubmitEvent.MethodType.POST;
 import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
@@ -43,184 +42,183 @@ import de.uni.stuttgart.ipvs.tdl.database.MongoDBConnector;
 @RestController
 @RequestMapping(value = "catalogue")
 public class TDLRestController {
-	/**
-	 * Database connector.
-	 */
-	public MongoDBConnector dbConnector = new MongoDBConnector();
-
-	/**
-	 * Validator
-	 */
-	private static final JsonValidator VALIDATOR = JsonSchemaFactory.byDefault().getValidator();
+    /**
+     * Database connector.
+     */
+    public MongoDBConnector dbConnector = new MongoDBConnector();
 
     /**
-     * TDL scheme as node
+     * Validator
      */
-	private JsonNode schemeNode = null;
+    private static final JsonValidator VALIDATOR = JsonSchemaFactory.byDefault().getValidator();
 
-	/**
-	 * Inserts a new topic to the database.
-	 * 
-	 * @param tdlJSON
-	 *            topic description
-	 * @return the id of the topic, which is provided by the database
-	 */
-	@RequestMapping(method = POST, value = "/add")
-	@ResponseBody
-	public String addNewTopic(@RequestBody String topicDescription) {
-		try {
-			this.schemeNode = JsonLoader.fromFile(new File("src/main/resources/jsonTDLscheme.json"));
-			JsonNode newTopicNode = JsonLoader.fromString(topicDescription);
-			ProcessingReport proRep = VALIDATOR.validate(schemeNode, newTopicNode);
-			if (proRep.isSuccess()) {
-				return dbConnector.storeTopicDescription(topicDescription);
-			} else {
-				List messages = Lists.newArrayList(proRep);
-				for (Object message : messages) {
-					System.out.println(message.toString());
-				}
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (ProcessingException e) {
-			e.printStackTrace();
-		}
-		return HttpStatus.BAD_REQUEST.toString();
+    /**
+     * Inserts a new topic to the database.
+     *
+     * @return the id of the topic, which is provided by the database
+     */
+    @RequestMapping(method = POST, value = "/add")
+    @ResponseBody
+    public ResponseEntity addNewTopic(@RequestBody String topicDescription) {
+        StringBuilder badRequestMsg = new StringBuilder();
+        try {
+            JsonNode schemeNode = JsonLoader.fromFile(new File("src/main/resources/jsonTDLscheme.json"));
+            JsonNode newTopicNode = JsonLoader.fromString(topicDescription);
+            ProcessingReport proRep = VALIDATOR.validate(schemeNode, newTopicNode);
+            if (proRep.isSuccess()) {
+                return ResponseEntity.ok().body(dbConnector.storeTopicDescription(topicDescription));
+            } else {
+                List messages = Lists.newArrayList(proRep);
+                for (Object message : messages) {
+                    badRequestMsg.append(message.toString());
+                }
+            }
+        } catch (IOException | ProcessingException e) {
+            e.printStackTrace();
+        }
+        return ResponseEntity
+                .badRequest()
+                .body(badRequestMsg.toString());
 
-	}
+    }
 
-	/**
-	 * Deletes a topic based on the given topic id.
-	 * 
-	 * @param id
-	 *            topic description id
-	 * @return Status code 200, if successful, 500 else
-	 */
-	@RequestMapping(method = DELETE, value = "/delete/{id}")
-	@ResponseBody
-	public ResponseEntity<HttpStatus> deleteTopic(@PathVariable String id) {
-		if (dbConnector.deleteTopicDescription(id)) {
-			return new ResponseEntity<HttpStatus>(HttpStatus.OK);
-		} else {
-			return new ResponseEntity<HttpStatus>(HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-	}
+    /**
+     * Deletes a topic based on the given topic id.
+     *
+     * @param id topic description id
+     * @return Status code 200, if successful, 500 else
+     */
+    @RequestMapping(method = DELETE, value = "/delete/{id}")
+    @ResponseBody
+    public ResponseEntity<HttpStatus> deleteTopic(@PathVariable String id) {
+        if (dbConnector.deleteTopicDescription(id)) {
+            return new ResponseEntity<HttpStatus>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<HttpStatus>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
-	/**
-	 * Updates the attributes of a given topic.
-	 * 
-	 * @param id
-	 *            topic description id
-	 * @param tdlAttributes
-	 *            attributes, which have to be changed
-	 * @return status code 200, if successful, 500 else
-	 * 
-	 */
-	@RequestMapping(method = PUT, value = "/update/{id}")
-	@ResponseBody
-	public ResponseEntity<HttpStatus> updateTopic(@PathVariable String id, @RequestBody String tdlAttributes) {
-		Map<String, String> updateParameter = new HashMap<String, String>();
-		try {
-			JSONObject updateParameterJson = new JSONObject(tdlAttributes);
-			Iterator<String> keysIterator = updateParameterJson.keys();
-			// Iterate over all update parameter
-			while (keysIterator.hasNext()) {
-				String key = (String) keysIterator.next();
-				updateParameter.put(key, updateParameterJson.getString(key));
-			}
-			if (dbConnector.updateTopicDescription(id, updateParameter)) {
-				return new ResponseEntity<HttpStatus>(HttpStatus.ACCEPTED);
-			} else {
-				return new ResponseEntity<HttpStatus>(HttpStatus.INTERNAL_SERVER_ERROR);
-			}
-		} catch (JSONException e) {
-			return new ResponseEntity<HttpStatus>(HttpStatus.BAD_REQUEST);
-		}
+    /**
+     * Updates the attributes of a given topic.
+     *
+     * @param id            topic description id
+     * @param tdlAttributes attributes, which have to be changed
+     * @return status code 200, if successful, 400 and 500 else
+     */
+    @RequestMapping(method = PUT, value = "/update/{id}")
+    @ResponseBody
+    public ResponseEntity<HttpStatus> updateTopic(@PathVariable String id, @RequestBody String tdlAttributes) {
+        Map<String, String> updateParameter = new HashMap<String, String>();
+        try {
+            JsonNode schemeNode = JsonLoader.fromFile(new File("src/main/resources/jsonTDLscheme.json"));
+            JsonNode newTopicNode = JsonLoader.fromString(tdlAttributes);
+            ProcessingReport proRep = VALIDATOR.validate(schemeNode, newTopicNode);
 
-	}
+            if (proRep.isSuccess()) {
+                JSONObject updateParameterJson = new JSONObject(tdlAttributes);
+                Iterator<String> keysIterator = updateParameterJson.keys();
+                // Iterate over all update parameter
+                while (keysIterator.hasNext()) {
+                    String key = (String) keysIterator.next();
+                    updateParameter.put(key, updateParameterJson.getString(key));
+                }
+                if (dbConnector.updateTopicDescription(id, updateParameter)) {
+                    return new ResponseEntity<>(HttpStatus.ACCEPTED);
+                } else {
+                    return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+                }
+            } else {
+                List messages = Lists.newArrayList(proRep);
+                for (Object message : messages) {
+                    System.out.println(message.toString());
+                }
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+        } catch (JSONException | IOException | ProcessingException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
 
-	/**
-	 * Searches for topics based on attributes.
-	 * 
-	 * @param filters
-	 *            tag map of attributes, which have to match with all topics of
-	 *            the result list
-	 * @return list of matching topic descriptions
-	 * 
-	 *         We directly throw the JSON exception to the user :)
-	 */
-	@RequestMapping(method = POST, value = "/search", produces = "application/json; charset=utf-8")
-	@ResponseBody
-	public ResponseEntity searchTopics(@RequestBody String filters) {
-		HashMap<String, String> filterMap = new HashMap<String, String>();
-		JSONObject filterJson;
-		try {
-			filterJson = new JSONObject(filters);
-			if (filterJson.has("filters")) {
-				filterJson = filterJson.getJSONObject("filters");
-			} else {
-				return new ResponseEntity<HttpStatus>(HttpStatus.BAD_REQUEST);
-			}
-			Iterator<String> keysIterator = filterJson.keys();
+    }
 
-			while (keysIterator.hasNext()) {
-				String key = (String) keysIterator.next();
+    /**
+     * Searches for topics based on attributes.
+     *
+     * @param filters tag map of attributes, which have to match with all topics of
+     *                the result list
+     * @return list of matching topic descriptions
+     * <p>
+     * We directly throw the JSON exception to the user :)
+     */
+    @RequestMapping(method = POST, value = "/search", produces = "application/json; charset=utf-8")
+    @ResponseBody
+    public ResponseEntity searchTopics(@RequestBody String filters) {
+        HashMap<String, String> filterMap = new HashMap<String, String>();
+        JSONObject filterJson;
+        try {
+            filterJson = new JSONObject(filters);
+            if (filterJson.has("filters")) {
+                filterJson = filterJson.getJSONObject("filters");
+            } else {
+                return new ResponseEntity<HttpStatus>(HttpStatus.BAD_REQUEST);
+            }
+            Iterator<String> keysIterator = filterJson.keys();
 
-				// Check if there is a nested JSONObject
-				if (filterJson.optJSONObject(key) != null) {
-					JSONObject childJson = filterJson.getJSONObject(key);
-					Iterator<String> childKeysIterator = childJson.keys();
+            while (keysIterator.hasNext()) {
+                String key = (String) keysIterator.next();
 
-					// Iterate over child keys and put the keys together
-					while (childKeysIterator.hasNext()) {
-						String childKey = (String) childKeysIterator.next();
-						filterMap.put(key + "." + childKey, childJson.getString(childKey));
-					}
-				} else {
-					filterMap.put(key, filterJson.getString(key));
-				}
-			}
+                // Check if there is a nested JSONObject
+                if (filterJson.optJSONObject(key) != null) {
+                    JSONObject childJson = filterJson.getJSONObject(key);
+                    Iterator<String> childKeysIterator = childJson.keys();
 
-			List<String> descriptionList = dbConnector.getMatchedTopicDescriptions(filterMap);
-			JSONArray topicDescriptionJsonArray = new JSONArray();
-			for (String topicDescription : descriptionList) {
-				topicDescriptionJsonArray.add(topicDescription);
-			}
-			return new ResponseEntity<>(topicDescriptionJsonArray, HttpStatus.OK);
-		} catch (JSONException e) {
-			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-		}
+                    // Iterate over child keys and put the keys together
+                    while (childKeysIterator.hasNext()) {
+                        String childKey = (String) childKeysIterator.next();
+                        filterMap.put(key + "." + childKey, childJson.getString(childKey));
+                    }
+                } else {
+                    filterMap.put(key, filterJson.getString(key));
+                }
+            }
 
-	}
+            List<String> descriptionList = dbConnector.getMatchedTopicDescriptions(filterMap);
+            JSONArray topicDescriptionJsonArray = new JSONArray();
+            for (String topicDescription : descriptionList) {
+                topicDescriptionJsonArray.add(topicDescription);
+            }
+            return new ResponseEntity<>(topicDescriptionJsonArray, HttpStatus.OK);
+        } catch (JSONException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
 
-	/**
-	 * Returns a topic with the provided id.
-	 * 
-	 * @param id
-	 *            topic description id
-	 * @return topic description
-	 */
-	@RequestMapping(method = GET, value = "/get/{id}", produces = "application/json; charset=utf-8")
-	@ResponseBody
-	public ResponseEntity<String> getTopic(@PathVariable String id) {
-		String topicDescription = dbConnector.getMatchedTopicDescription(id);
-		if (null != topicDescription) {
-			return new ResponseEntity<String>(topicDescription, HttpStatus.OK);
-		} else {
-			return new ResponseEntity<String>("{}", HttpStatus.OK);
-		}
-	}
+    }
 
-	/**
-	 * Header information for swagger requests.
-	 *
-	 * @param response
-	 *            Response information
-	 */
-	@ModelAttribute
-	public void setVaryResponseHeader(HttpServletResponse response) {
-		response.setHeader("Access-Control-Allow-Origin", "*");
-		response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	}
+    /**
+     * Returns a topic with the provided id.
+     *
+     * @param id topic description id
+     * @return topic description
+     */
+    @RequestMapping(method = GET, value = "/get/{id}", produces = "application/json; charset=utf-8")
+    @ResponseBody
+    public ResponseEntity<String> getTopic(@PathVariable String id) {
+        String topicDescription = dbConnector.getMatchedTopicDescription(id);
+        if (null != topicDescription) {
+            return new ResponseEntity<String>(topicDescription, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<String>("{}", HttpStatus.OK);
+        }
+    }
+
+    /**
+     * Header information for swagger requests.
+     *
+     * @param response Response information
+     */
+    @ModelAttribute
+    public void setVaryResponseHeader(HttpServletResponse response) {
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    }
 
 }
